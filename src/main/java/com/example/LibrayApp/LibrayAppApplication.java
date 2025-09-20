65149c6f88b19a7c72f347722af8da9cc16ddea1
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-09-20 10:15:06 +0200

    creata initail phase for libray application

---
+++ src/main/java/com/example/LibrayApp/LibrayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibrayApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrayAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LibrayApp/LibrayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibrayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
